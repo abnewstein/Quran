75654fddbc176e21295a4c953cb1bca47cef763c
Abdul Rahman <abnewstein@gmail.com>
2023-10-15 22:21:34 -0400

    wip

---
+++ Quran.Web/Client.fs
@@ -0,0 +1,31 @@
+﻿namespace Quran.Web
+
+open WebSharper
+open WebSharper.UI
+open WebSharper.UI.Templating
+open WebSharper.UI.Notation
+open Quran
+
+[<JavaScript>]
+module Templates =
+
+    type MainTemplate = Templating.Template<"Main.html", ClientLoad.FromDocument, ServerLoad.WhenChanged>
+
+[<JavaScript>]
+module Client =
+
+    let Main () =
+        let quran: Var<array<Quran>> = Var.Create [||]
+
+        printfn "%A" quran
+
+        Templates.MainTemplate.MainForm()
+            .OnSend(fun e ->
+                async {
+                    let! res = Server.getQuranData ()
+                    printfn "%A" res
+                    quran := res
+                }
+                |> Async.StartImmediate
+            )            
+            .Doc()
---
+++ Quran.Web/Remoting.fs
@@ -0,0 +1,13 @@
+﻿namespace Quran.Web
+
+open WebSharper
+open Quran
+
+module Server =
+
+    [<Rpc>]
+    let getQuranData () =
+        let quran = Service.getAvailableQuranData()
+        async {
+            return quran
+        }
